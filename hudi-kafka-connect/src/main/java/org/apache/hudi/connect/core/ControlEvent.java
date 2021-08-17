8cb1acd9140eafd7755a35d556559b199c96a863
Rajesh Mahindra <rmahindra@Rajeshs-MacBook-Pro.local>
2021-08-16 17:16:44 -0700

    Initial commit for hudi kafka connect

---
+++ hudi-kafka-connect/src/main/java/org/apache/hudi/connect/core/ControlEvent.java
@@ -0,0 +1,173 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hudi.connect.core;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * The events sent over the Kafka Control Topic between the 
+ * coordinator and the followers, in order to ensure 
+ * coordination across all the writes.
+ */
+@SuppressWarnings("checkstyle:VisibilityModifier")
+public class ControlEvent implements Serializable {
+
+  private MsgType msgType;
+  private long commitTime;
+  private int senderPartition;
+  private CoordinatorInfo coordinatorInfo;
+  private ParticipantInfo participantInfo;
+
+  public ControlEvent() {
+  }
+
+  public ControlEvent(MsgType msgType,
+                      long commitTime,
+                      int senderPartition,
+                      CoordinatorInfo coordinatorInfo,
+                      ParticipantInfo participantInfo) {
+    this.msgType = msgType;
+    this.commitTime = commitTime;
+    this.senderPartition = senderPartition;
+    this.coordinatorInfo = coordinatorInfo;
+    this.participantInfo = participantInfo;
+  }
+
+  public String key() {
+    return msgType.name().toLowerCase(Locale.ROOT);
+  }
+
+  public MsgType getMsgType() {
+    return msgType;
+  }
+
+  public long getCommitTime() {
+    return commitTime;
+  }
+
+  public int getSenderPartition() {
+    return senderPartition;
+  }
+
+  public CoordinatorInfo getCoordinatorInfo() {
+    return coordinatorInfo;
+  }
+
+  public ParticipantInfo getParticipantInfo() {
+    return participantInfo;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s %s %s %s %s", msgType.name(), commitTime, senderPartition, coordinatorInfo.toString(), participantInfo.toString());
+  }
+
+  /**
+   * Builder that helps build {@link ControlEvent}.
+   */
+  public static class Builder {
+
+    private final MsgType msgType;
+    private final long commitTime;
+    private final int senderPartition;
+    private CoordinatorInfo coordinatorInfo;
+    private ParticipantInfo participantInfo;
+
+    public Builder(MsgType msgType, long commitTime, int senderPartition) {
+      this.msgType = msgType;
+      this.commitTime = commitTime;
+      this.senderPartition = senderPartition;
+    }
+
+    public Builder setCoodinatorInfo(CoordinatorInfo coordinatorInfo) {
+      this.coordinatorInfo = coordinatorInfo;
+      return this;
+    }
+
+    public Builder setParticipantInfo(ParticipantInfo participantInfo) {
+      this.participantInfo = participantInfo;
+      return this;
+    }
+
+    public ControlEvent build() {
+      return new ControlEvent(msgType, commitTime, senderPartition, coordinatorInfo, participantInfo);
+    }
+  }
+
+  /**
+   * The info sent by the {@link TransactionCoordinator} to one or more
+   * {@link TransactionParticipant}s.
+   */
+  public static class CoordinatorInfo implements Serializable {
+
+    private Map<Integer, Long> globalKafkaCommitOffsets;
+
+    public CoordinatorInfo() {
+    }
+
+    public CoordinatorInfo(Map<Integer, Long> globalKafkaCommitOffsets) {
+      this.globalKafkaCommitOffsets = globalKafkaCommitOffsets;
+    }
+
+    public Map<Integer, Long> getGlobalKafkaCommitOffsets() {
+      return (globalKafkaCommitOffsets == null) ? new HashMap<>() : globalKafkaCommitOffsets;
+
+    }
+  }
+
+  /**
+   * The info sent by a {@link TransactionParticipant} instances to the
+   * {@link TransactionCoordinator}.
+   */
+  public static class ParticipantInfo implements Serializable {
+
+    private long kafkaCommitOffset;
+    private OutcomeType outcomeType;
+
+    public ParticipantInfo() {
+    }
+
+    public ParticipantInfo(long kafkaCommitOffset, OutcomeType outcomeType) {
+      this.kafkaCommitOffset = kafkaCommitOffset;
+      this.outcomeType = outcomeType;
+    }
+
+    public long getKafkaCommitOffset() {
+      return kafkaCommitOffset;
+    }
+
+    public OutcomeType getOutcomeType() {
+      return outcomeType;
+    }
+  }
+
+  enum MsgType {
+    START_COMMIT,
+    END_COMMIT,
+    ACK_COMMIT,
+    WRITE_STATUS,
+  }
+
+  enum OutcomeType {
+    WRITE_SUCCESS,
+  }
+}
---
+++ hudi-kafka-connect/src/main/java/org/apache/hudi/connect/writers/LocalCommitFile.java
@@ -0,0 +1,126 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hudi.connect.writers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class LocalCommitFile implements Serializable {
+
+  public static final String BASE_PATH = "/tmp";
+  private static final String COMMIT_FILE_PREFIX = ".hudi";
+  private static final String COMMIT_FILE_EXT = "commit";
+  private static final Logger LOG = LoggerFactory.getLogger(LocalCommitFile.class);
+
+  private final long commitTime;
+  private final int numPartitions;
+  private final List<String> filesWritten;
+  private final Map<Integer, Long> kafkaOffsets;
+  private final String filepath;
+
+  public LocalCommitFile(long commitTime, int numPartitions, List<String> filesWritten, String topicName, Map<Integer, Long> kafkaOffsets) {
+    this.commitTime = commitTime;
+    this.numPartitions = numPartitions;
+    this.filesWritten = filesWritten;
+    this.kafkaOffsets = kafkaOffsets;
+    File commitFile = new File(BASE_PATH,
+        String.format("%s-%s-%s.%s", COMMIT_FILE_PREFIX, commitTime, topicName, COMMIT_FILE_EXT));
+    filepath = commitFile.getPath();
+  }
+
+  public long getCommitTime() {
+    return commitTime;
+  }
+
+  public int getNumPartitions() {
+    return numPartitions;
+  }
+
+  public List<String> getFilesWritten() {
+    return filesWritten;
+  }
+
+  public Map<Integer, Long> getKafkaOffsets() {
+    return kafkaOffsets;
+  }
+
+  @Override
+  public String toString() {
+    return " Commit Time: " + this.commitTime
+        + " Number of Partitions : " + this.numPartitions
+        + " Files Written : " + String.join(",", this.filesWritten)
+        + " Kafka Offsets: " + this.kafkaOffsets.entrySet().stream()
+        .map(entry -> entry.getKey() + ":" + entry.getValue())
+        .collect(Collectors.joining(", ", "{", "}"));
+  }
+
+  public void doCommit() throws Exception {
+    FileOutputStream fileOut = new FileOutputStream(filepath);
+    ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+    objectOut.writeObject(this);
+    objectOut.close();
+    LOG.info("The file with path {} and commit time {} was written", filepath, commitTime);
+  }
+
+  public static LocalCommitFile getLatestCommit() throws IOException, ClassNotFoundException {
+    DirectoryStream<Path> directoryStream = listCommitFiles();
+    Path latestCommitFilePath = null;
+    long maxCommitTime = -1;
+    for (Path path : directoryStream) {
+      String[] pathComponents = path.toString().split("-");
+      long commitTime = Long.parseLong(pathComponents[1]);
+      if (commitTime > maxCommitTime) {
+        maxCommitTime = commitTime;
+        latestCommitFilePath = path;
+      }
+    }
+    if (latestCommitFilePath == null) {
+      throw new IOException("Error finding the commit file with the latest commit");
+    }
+    FileInputStream fileIn = new FileInputStream(latestCommitFilePath.toString());
+    ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+    LocalCommitFile latestCommitFile = (LocalCommitFile) objectIn.readObject();
+    objectIn.close();
+    LOG.info("Leader will use existing kafka offsets {} commited to the latest commit file {} ",
+        latestCommitFile.getKafkaOffsets(),
+        latestCommitFilePath.toString());
+    return latestCommitFile;
+  }
+
+  private static DirectoryStream<Path> listCommitFiles() throws IOException {
+    return Files.newDirectoryStream(
+        Paths.get(BASE_PATH), COMMIT_FILE_PREFIX + "*" + COMMIT_FILE_EXT);
+  }
+
+}
